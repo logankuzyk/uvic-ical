0d520b746e44de502d1f6d5ab9759c12a0ad0e6b
Logan Kuzyk <logan@kuzyk.net>
2021-09-22 17:31:43 -0700

    exports is not defined - content.js (something to do with ical-generator)

--- rollup.config.js
+++ rollup.config.js
@@ -7,17 +7,22 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import copy from "rollup-plugin-copy";
 import postcss from "rollup-plugin-postcss";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export default defineConfig([
   {
     plugins: [
-      typescript(),
+      nodePolyfills(),
       terser(),
       copy({ targets: [{ src: "public/*", dest: "build" }] }),
-      nodeResolve({ browser: true }),
       commonjs(),
+      nodeResolve({ browser: true, preferBuiltins: false }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
     ],
-    input: "src/content_scripts/calendar.ts",
+    input: "src/content_scripts/calendar.js",
     output: {
       file: "build/static/content.js",
       format: "iife",
@@ -30,15 +35,12 @@ export default defineConfig([
       nodeResolve({ browser: true }),
       postcss({ extensions: [".css"] }),
       commonjs(),
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("production"),
-        preventAssignment: true,
-      }),
     ],
     input: "src/index.tsx",
     output: {
       file: "build/static/min.js",
       format: "iife",
+      exports: "none",
     },
   },
 ]);
